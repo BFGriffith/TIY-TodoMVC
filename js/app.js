3112ad21e35345f33ca75133422a3b53385dd95f
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-05-15 16:23:20 -0400

    Friday afternoon progress, <li> elements...

--- js/app.js
+++ js/app.js
@@ -8,164 +8,87 @@
 //1. click on "what needs to be done?" (input class="newtodo")
 //2. type the thing you want to remember (task) ()
 //3. press the "Enter" key (input.newtodo)
-  //4. ROBOT: task should be added to dictionary array representng tasklist in controller
-  //5. ROBOT: remove task from the (input new-todo box)
-  //6. ROBOT: update the number of tasks in the footer / array
-  //7. ROBOT: task string should move from input to <ul.todo-list><li> (update visual UI)
-  //$('ul.input.new-todo') = selecting a class
-  //$('ul#todo-list') = ID
+//4. ROBOT: task should be added to dictionary array representng tasklist in controller
+//5. ROBOT: remove task from the (input new-todo box)
+//6. ROBOT: update the number of tasks in the footer / array
+//7. ROBOT: task string should move from input to <ul.todo-list><li> (update visual UI)
+//$('ul.input.new-todo') = selecting a class
+//$('ul#todo-list') = ID
 
 //task string name goes @ <label> AND value= (value of input.edit)
 //checkbox = input.toggle  ('input.toggle').value (input is a dictionary), toggles Boolean
 //debugger;
-(function (window) {
-	'use strict';
+(function(window) {
+  'use strict';
 
-	var addTask = document.querySelectorAll('input.new-todo');
-	  addTask.addEventListener('keyup', function EnterKey(event){
-			//given an HTML element <input class="new-todo">
-			if ( event.keyCode === 13 ){
-	      var task = document.querySelector('input.new-todo').value;
-				todos.taskList.push(task);
-				console.log(todos.taskList);
-			}
+  var newTodoInput = document.querySelector('input.new-todo');
+  addTask.addEventListener('keyup', function EnterKey(event) {
+    //given an HTML element <input class="new-todo">
+    if (event.keyCode === 13) {
+      var task = newTodoInput.value;
+      todos.addTaskToList(task, todo - list) {
+        console.log(todos.taskList);
+      }
+    }
 
-	var newTodoInput = document.querySelector('input.new-todo');
-	var deleteTask = document.querySelector('ul.todo-list');
-	var deleteTaskButtons = document.querySelectorAll('button.destroy');
-	var deleteTask.addEventListener('mouseover', function deleteTaskController(event)){
-		console.log("delete stuff!")
-	});
+    var newTodoInput = document.querySelector('input.new-todo');
+    var deleteTask = document.querySelector('ul.todo-list');
+    var deleteX = document.querySelectorAll('button.destroy');
 
-	document.querySelector('input.new-todo').value = "";
+    document.querySelector('input.new-todo').value = "";
 
-	// UPDATE the number of tasks in the footer
-	if (todos.taskList.length === 1) {
-		(document.querySelector('span.todo-count').textContent = (todos.taskList.length + " Item Left"));
-	} else {
-		(document.querySelector('span.todo-count').textContent = (todos.taskList.length + " Items Left"));
-	};
+		//number of tasks in footer
+    function() {
+      var todoCountText = todos.taskList.length + ' Item';
+      if (todos.taskList.length === 1) {
+        todoCountText += ' Left';
+      } else {
+        todoCountText += 's Left';
+      };
+    }
 
-	console.log(todos.taskList);
+		todoCountElement.text = todoCountText;
 
-	_.forEach(deleteTaskButtons, function(element, index, deleteTaskButtons){
-		element.addEventListener('click', function(){
-			console.log("delete crap!");
-		});
-	});
+		document.querySelector('ul.todo-list').innerHTML += (
+			"<li> new task: " + task +
+			"</li>"
+		)
 
-	_.forEach(editTask, function(element, index, editTask){
-		element.addEventListener('dblclick', function(){
-			console.log("edit this shit?");
-		})
-	})
+    console.log(todos.taskList);
 
-});//end of addEventListener post EnterKey function
+    _.forEach(deleteX, function(element, index, deleteX) {
+      element.addEventListener('click', function removeLi() {
+        var deleteX = document.querySelectorAll
+        console.log("delete crap!");
+      });
+    });
+
+    _.forEach(editTask, function(element, index, editTask) {
+      element.addEventListener('dblclick', function() {
+        console.log("edit this shit?");
+      })
+    })
+
+  });
 
 })(window);
 
+// CONTROLLER FOR: I can edit a task on my list...
+// 1. What event should I be listening for?
+// 2. What element makes sense to listen for that event?
+// 3. What do I need to do when that event fires?
 
+// CONTROLLER FOR: I can complete a task on my list...
+// 1. What event should I be listening for? (double click, keyup [Enter, tab, esc])
+// 2. What element makes sense to listen for that event? ('ul.todo-list')
+// 3. What do I need to do when that event fires?
 
+// CONTROLLER FOR: I can view the list...
+// 1. What event should I be listening for?
+// 2. What element makes sense to listen for that event?
+// 3. What do I need to do when that event fires?
 
-
-
-
-
-
-//simplified?
-/*
-document.querySelectorAll('input.new-todo');
-  addEventListener('keyup', function(){
-});
-*/
-/*
-
-(function (window) {
-	'use strict';
-	var list = [ ];
-
-	todos.addTaskToLisgitt("Remember the beer", list);
-	console.log(list);
-
-document.head.parentElement.className="js"; // It's alive!
-
-var elements = document.querySelectorAll('.cbp-nttrigger');
-
-var elements = document.querySelectorAll('ul');
-var listItem = document.querySelectorAll('li');
-
-_.forEach(elements, function(element, index, elements){
-	  element.addEventListener('click', function(){
-	    elements[index].parentElement.classList.toggle('todo-list');
-	  });
-	});
-
-	_.forEach(listItem, function(element, index, elements){
-	    element.addEventListener('click', function(){
-	        element.parentElement.classList.toggle('todo-list');
-	    });
-
-document.querySelector('.new-todo');
-document.getElementsByClassName('new-todo');
-
-$('input.new-todo').addEventListener('keypress', function(){
-	console.log(arguments);
-});
-$('input.new-todo').addEventListener('keyup', function(){
-	console.log(arguments);
-});
-$('input.new-todo').addEventListener('keydown', function(){
-	console.log(arguments);
-});
-document.body.addEventListener('keydn', function(){
-	console.log(arguments)
-});
-
-addTaskToList("Remember the beer", list);
-
-/*
-var addTask = document.querySelector('header')
-	var that = this;
-	this.ENTER_KEY = 13;
-
-	addTask.addEventListener(this.ENTER_KEY, function view(template){
-		console.log('test');
-	})
-*/
-
-/*
-var nestedElements = document.querySelectorAll('h4');
-elements[0].addEventListener('click', function(){
-	elements[0].parentElement.classList.toggle('cbp-ntopen');
-});
-
-elements[1].addEventListener('click', function(){
-	elements[1].parentElement.classList.toggle('cbp-ntopen');
-});
-*/
-
-/*
-for ( var index = 0; index < elements.length; index++ ){
-		var element = elements[index]
-		element[index].addEventListener('click', function(){
-				console.log(element, index, element[index])
-				element[index].parentElement.classList.toggle('cbp-ntopen');
-		});
-}*/
-
-/*
-_.forEach(elements, function(element, index, elements){
-		element.addEventListener('click', function(){
-			var addTask = document.querySelector('header')
-		});
-});
-*/
-
-/*
-_.forEach(nestedElements, function(element, index, nestedElements){
- 		element.addEventListener('click', function(){
- 				element.parentElement.classList.toggle('cbp-ntopen');
- 		});
-});
-*/
-//})(window);
+// CONTROLLER FOR: I can view the list...
+// 1. What event should I be listening for?
+// 2. What element makes sense to listen for that event?
+// 3. What do I need to do when that event fires?
--- js/todo.js
+++ js/todo.js
@@ -2,56 +2,47 @@
 //model part of MVC structure
 
 //I can list my tasks
-(function (window){
-  var taskList = [ ];
+(function(window) {
+  var taskList = [];
 
   // I can list my tasks...
-  function listTasks(){
-        // TODO: make this do something....
-      }
+  function listTasks() {
+    // TODO: make this do something....
+  }
 
   // I can add a task to my list...
-  function addTaskToList(task,list){
-      // What is the task? @param task
-      // Where is the task going? @param list
-      // What order / priority? lowest, on the bottom
-      return list.push(task);
-      // add task to list somehow on the bottom
+  function addTaskToList(task, list) {
+    return list.push(task);
+    // add task to list somehow on the bottom
   }
 
   // window.addTaskToList = addTaskToList;
 
   //I can edit a task on my list...
-  function editTask(position,newTaskName){
-      // Which task is being edited? @param position
-      // What is the edit? @param newTaskName
-      // New task name should be displayed on task list
-      return taskList[position] = newTaskName;
+  function editTask(position, newTaskName) {
+    return taskList[position] = newTaskName;
   }
 
   // I can delete a task on my list...
-  function deleteTask(task,list){
-      // Which task is being deleted? @param
-      // Where is it being deleted from? @param
-      // How does the rest of the list change after deleting?
-      return list.splice(task, 1);
+  function deleteTask(task, list) {
+    return list.splice(task, 1);
   }
 
-  function completeTask(task, list){
-      // Which task is being completed?
-      // Where is the task being marked as completed?
-      return list[task - 1] += " COMPLETE";
+  function completeTask(task, list) {
+    // Which task is being completed?
+    // Where is the task being marked as completed?
+    return list[task - 1] += " COMPLETE";
   }
   window.todos = {
-          "taskList": taskList,
-          "addTaskToList": addTaskToList,
-          "completeTask": completeTask,
-          "deleteTask": deleteTask
-          };
+    "taskList": taskList,
+    "addTaskToList": addTaskToList,
+    "completeTask": completeTask,
+    "deleteTask": deleteTask
+  };
 
 })(window);
 
-  //function invocation:
+//function invocation:
 /*
   //I can add a task to my list?
   function addTask(task, tasklist){
